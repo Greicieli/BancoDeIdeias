dd6551bd2a6bd345acfaab1840ff90580a2707c6
greici <greicicassanelli@gmail.com>
2015-09-28 22:24:27 -0300

    Realizado Crud

---
+++ banco_de_ideias/src/model/UserRn.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.util.List;
+
+import Dominio.User;
+import dao.UserDao;
+
+public class UserRn {
+	
+	private UserDao dao;
+	
+	public UserRn() {
+		dao = new UserDao();
+	}
+
+	public void salvar(User user) throws Exception {
+		if (user.getNome().trim().isEmpty()) {
+			throw new Exception("O nome é obrigatório!");
+		}
+
+		if (user.getEmail().trim().isEmpty()) {
+			throw new Exception("O E-mail é obrigatório!");
+		}
+		
+		dao.salvar(user);
+	}
+
+	public List<User> listar() {
+		return dao.listarTodos();
+	}
+
+	public User buscarPorId(Long id) {
+		return dao.buscarPorId(id);
+	}
+
+	public void excluir(Long id) throws Exception {
+		dao.excluir(id);
+	}
+
+}
+

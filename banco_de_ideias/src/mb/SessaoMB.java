9fbb1444e063fe7f20e25b80f918480eb39b962e
greici <greicicassanelli@gmail.com>
2015-10-26 19:48:29 -0200

    implementado crud de login

---
+++ banco_de_ideias/src/mb/SessaoMB.java
@@ -0,0 +1,57 @@
+package mb;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import model.UserRn;
+import Dominio.User;
+
+@SessionScoped
+@ManagedBean
+public class SessaoMB {
+	private User usuarioLogado;
+	
+	private UserRn rn;
+
+	private User usuarioForm;
+
+	@PostConstruct
+	public void iniciar() {
+		usuarioForm = new User();
+	}
+
+	public User getUsuarioForm() {
+		return usuarioForm;
+	}
+
+	public void setUsuarioForm(User usuarioForm) {
+		this.usuarioForm = usuarioForm;
+	}
+
+	public String entrar() {
+		rn = new UserRn();
+		User usuario = rn.buscarPorEmail(usuarioForm.getEmail());
+		
+		if ( usuario != null && usuarioForm.getEmail().equalsIgnoreCase(usuario.getEmail())
+				&& usuarioForm.getSenha().equals(usuario.getSenha())) {
+			usuarioLogado = usuario;
+			System.out.println("Entrou");
+			return "/index";
+			
+		}
+		
+		System.out.println("Não Entrou");
+		return "";
+	}
+
+	public String sair() {
+		usuarioLogado = null;
+		return "";
+	}
+
+	public Boolean estaLogado() {
+		return usuarioLogado != null;
+	}
+
+}
